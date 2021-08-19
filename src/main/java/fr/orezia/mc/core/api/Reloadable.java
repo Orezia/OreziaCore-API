7de06acf245d9f07042d44da31a7cd5eeb8b3f93
dederobert <adinquer@yahoo.com>
2021-08-19 21:55:39 +0200

    Create OreziaCore-api

---
+++ src/main/java/fr/orezia/mc/core/api/Reloadable.java
@@ -0,0 +1,15 @@
+package fr.orezia.mc.core.api;
+
+/**
+ * Mark class as reloadable, meaning it has store data which may need to be reloaded.
+ *
+ * @since 4.0
+ */
+public interface Reloadable {
+
+  /**
+   * Reload stored data.
+   */
+  void reload();
+
+}
