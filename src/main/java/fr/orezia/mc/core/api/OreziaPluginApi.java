0f9d2c5f09a24d9ec6f15b1356549d2f2cd01f28
dederobert <adinquer@yahoo.com>
2021-08-20 21:12:00 +0200

    Fix dependencies

---
+++ src/main/java/fr/orezia/mc/core/api/OreziaPluginApi.java
@@ -0,0 +1,21 @@
+package fr.orezia.mc.core.api;
+
+import fr.orezia.mc.core.api.annotation.PublicApi;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * API for orezia plugin.
+ * @since 1.0
+ */
+@PublicApi
+public interface OreziaPluginApi {
+
+  /**
+   * Get the plugin name.
+   *
+   * @return The plugin name.
+   */
+  @PublicApi
+  @NotNull String pluginName();
+
+}
