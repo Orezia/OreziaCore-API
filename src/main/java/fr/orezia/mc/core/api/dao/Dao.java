a5d14f6c353a129ecd3f90bd34c9ebfefc9bc8cf
Alexis DINQUER <adinquer@yahoo.com>
2021-10-27 21:24:16 +0200

    API pour les DAO (#4)
    * Fix embed dependencies handling
    
    For embed dependencies, use `johnrengelman.shadow` plugin and avoid embed paper API.
    
    * Update DAO API
    
    - Move DAO from core to API
    - Replace `EntityDao` by interface `IdentifiableWithTechnicalId`
    - Replace `BusinessEntityDao` by interface `IdentifiableWithBusinessId`
    - Create `Cascade` annotation

---
+++ src/main/java/fr/orezia/mc/core/api/dao/Dao.java
@@ -0,0 +1,33 @@
+package fr.orezia.mc.core.api.dao;
+
+import fr.orezia.mc.core.api.annotation.PublicApi;
+import java.util.List;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Dao.
+ *
+ * @param <T> The type of handled entity.
+ * @since 1.0
+ */
+public interface Dao<T> {
+
+
+  /**
+   * Retrieve, from persistance, all entities.
+   *
+   * @return The list of found entities
+   */
+  @PublicApi
+  @NotNull List<T> getAll();
+
+  /**
+   * Update, in persistance, the entity.
+   *
+   * @param object The new entity value.
+   * @return True if entity is updated, false if not.
+   */
+  @PublicApi
+  boolean update(final @NotNull T object);
+
+}
---
+++ src/main/java/fr/orezia/mc/core/api/entity/IdentifiableWithTechnicalId.java
@@ -0,0 +1,33 @@
+package fr.orezia.mc.core.api.entity;
+
+import fr.orezia.mc.core.api.annotation.PublicApi;
+import org.jetbrains.annotations.Contract;
+
+/**
+ * Entity identifiable with a technical ID.
+ *
+ * @since 1.0
+ */
+public interface IdentifiableWithTechnicalId {
+
+
+  /**
+   * Get the entity's technical ID.
+   *
+   * @return The technical ID.
+   */
+  @PublicApi
+  @Contract(pure = true)
+  int id();
+
+  /**
+   * Set the entity's technical ID.
+   *
+   * @param newId The new technical ID.
+   * @return The entity itself.
+   */
+  @PublicApi
+  @Contract(value = "_ -> this", mutates = "this")
+  IdentifiableWithTechnicalId id(final int newId);
+
+}
