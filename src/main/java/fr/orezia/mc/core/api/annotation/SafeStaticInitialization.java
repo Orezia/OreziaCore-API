e8836de6adde1d56dbd27f34f55e06ff2fb4d68f
Alexis DINQUER <adinquer@yahoo.com>
2022-02-25 19:47:11 +0100

    feat: Annotations `@SafeStaticInitialization` & `@UnsafeStaticInitialization` (#21)

---
+++ src/main/java/fr/orezia/mc/core/api/annotation/SafeStaticInitialization.java
@@ -0,0 +1,17 @@
+package fr.orezia.mc.core.api.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Indicates that the static field or method is <b>safe</b> to use in static initialization context.
+ *
+ * @since 1.0
+ */
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.METHOD, ElementType.FIELD})
+public @interface SafeStaticInitialization {
+
+}
